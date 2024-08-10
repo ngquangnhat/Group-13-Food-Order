aaaf7687a5a4724fce1a0f69e16ef306b67c1695
Nguyen Quang Nhat <nhatdzkk2211@gmail.com>
2024-08-11 02:24:14 +0700

    BTL

---
+++ app/src/main/java/com/example/foodorder/constant/AboutUsConfig.java
@@ -0,0 +1,18 @@
+package com.example.foodorder.constant;
+
+public interface AboutUsConfig {
+    String ABOUT_US_TITLE = "Group 13";
+    String ABOUT_US_CONTENT = "Học phần: Phát triển ứng dụng trên thiết bị di động \n " +"Mã lớp: 20234IT6029001 \n" +
+            "GVHD: ThS. Nguyễn Thái Cường.";
+    String ABOUT_US_SLOGAN = "HaNoi University Of Industry";
+    String ABOUT_US_WEBSITE_TITLE = "Our University";
+
+    String PAGE_FACEBOOK = "fb://page/haui";
+    String LINK_FACEBOOK = "https://www.facebook.com/Ng.QuangNhat.22.11.2003";
+    String LINK_YOUTUBE = "https://www.youtube.com/@nguyenquangnhat6885";
+    String PHONE_NUMBER = "0988710831";
+    String GMAIL = "quangnhat2211dev@gmail.com";
+    String SKYPE_ID = "dangtin29932";
+    String ZALO_LINK = "https://zalo.me/g/wzpznh002";
+    String WEBSITE = "https://www.haui.edu.vn/vn";
+}
